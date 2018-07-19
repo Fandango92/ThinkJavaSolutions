a3b9cf9525ad78d13dbaac04e672269e3bae5ebb
Fandango92 <joesingleton92@hotmail.com>
2018-07-19 01:47:43 +0100

    Create Hello.java

---
+++ Chapter 1 exercises/Hello.java
@@ -0,0 +1,13 @@
+
+
+public class Hello {
+	  // main method generates some simple output
+    public static void main(String[] args) {
+      
+        System.out.println("Hello, World!");
+		
+		System.out.println("How are You?");
+		
+    }
+
+}
